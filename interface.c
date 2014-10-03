0c6223684e32c37dc05c41f97c5eef3b2f755350
luckydonald <luckydonald@flutterb.at>
2014-10-03 14:04:20 +0200

    Fix for #1, tg-cli is keeping [100%Up] as prompt suffix after picture upload?

--- interface.c
+++ interface.c
@@ -469,12 +469,10 @@ char *get_default_prompt (void) {
       l += snprintf (buf + l, 999 - l, "%lld%%Down", 100 * tgl_state.cur_downloaded_bytes / tgl_state.cur_downloading_bytes);
     }
     l += snprintf (buf + l, 999 - l, "]" COLOR_NORMAL);
-    if (tgl_state.unread_messages) { // ADDED
-        l += snprintf (buf + l, 999 - l, "\n"); // ADDED
-    } // ADDED
+    l += snprintf (buf + l, 999 - l, "\n"); // ADDED
     return buf;
   } 
-  //l += snprintf (buf + l, 999 - l, "%s", default_prompt); // ADDED
+  l += snprintf (buf + l, 999 - l, "%s", default_prompt); // NOT ADDED
   return buf;
 }
 
